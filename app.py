8333751c505d4a1a248e4dcd5b2ff5a1971447bb
Sukeerth Balakrishna Kalluraya <sukeerthbalakrishnakalluraya@Sukeerths-MacBook-Air.local>
2023-03-22 23:28:09 -0600

    Simple web form for HW 5. DO NOT MERGE INTO MAIN

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+plans = [{"plan_title": "HW5 Submission", "plan_description": '"The work above is due by Thursday, March 22 2023 at 11:59pm. By that time, you should have completed the tasks above. We will review your user stories and confirm that we can access the simple web form you created for task 5."'}]
+
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    return "Hello World, from Team TBD!"
+    if request.method == 'POST':
+        temp = {}
+        for key, value in request.form.items():
+            print(key, value)
+            temp[key] = value
+        plans.append(temp)
+        print(plans)
+    return render_template('index.html', plans=plans)
