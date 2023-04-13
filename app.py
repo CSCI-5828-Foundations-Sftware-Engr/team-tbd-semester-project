e97cf806068037402524db34f7bffe9426a9e22d
Sukeerth Balakrishna Kalluraya <sukeerthbalakrishnakalluraya@Sukeerths-MacBook-Air.local>
2023-04-13 11:00:20 -0600

    Basic UI for log in, sign up, and forgot password pages

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
-from flask import Flask
+from flask import Flask, request, redirect, url_for, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return "Hello World, from Team TBD!"
+@app.route('/logIn', methods=['GET', 'POST'])
+def logIn():
+    error = None
+    if request.method == 'POST':
+        if request.form['email'] == 'admin@admin.com' and request.form['password'] == 'admin':
+            pass
+        else:
+            error = 'Invalid credentials. Please try again.'
+    return render_template('logIn.html', error=error)
+
+@app.route('/signUp', methods=['GET', 'POST'])
+def signUp():
+    if request.method == 'POST':
+        pass
+    return render_template('signUp.html')
+
+@app.route('/forgotPassword', methods=['GET', 'POST'])
+def forgotPassword():
+    if request.method == 'POST':
+        pass
+    return render_template('forgotPassword.html')
