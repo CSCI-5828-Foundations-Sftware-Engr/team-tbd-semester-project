bf30ef4ba83d6aa61703d5e05c31bba521658f77
Scott McCall <damc8609@colorado.edu>
2023-04-19 16:55:42 -0600

    updated reminders API

---
+++ server/reminders.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, Blueprint, jsonify
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity, get_jwt
+import db
+
+bp = Blueprint('reminders', __name__, url_prefix='/reminders')
+@bp.route('/get_reminders')
+@jwt_required()
+def return_data():
+    result = db.execute_query(f'SELECT * FROM reminders WHERE userid = "{get_jwt_identity()}";')
+    events = []
+    for event in result:
+        events.append(jsonify({"reminder_id": event[0], "title": event[2], "description": event[3], "start_time": event[4], "end_time": event[5], "reminder_type": event[6], "progress": event[7], "time_taken": event[8], "repeat_id": event[9]}))
+    return events, 200
+
+@bp.route('/add_event', methods= ['POST'])
+@jwt_required()
+def add_event():
+    title = request.form['title']
+    description = request.form['description']
+    start_time = request.form['start_time']
+    end_time = request.form['end_time']
+    reminder_type = request.form['reminder_type']
+    progress = request.form['progress']
+    db.execute_insert(f'INSERT INTO reminders(userid, title, description, start_time_stamp, end_time_stamp, reminder_type, progress) VALUES("{get_jwt_identity()}", "{title}", "{description}", "{start_time}", "{end_time}", "{reminder_type}", "{progress}");')
+    return "Event successfully added.", 200
+
+@bp.route('/delete_event', methods= ['POST', 'DELETE'])
+@jwt_required()
+def delete_event():
+    repeat_id = request.form['repeat_id']
+    reminder_id = request.form['reminder_id']
+    if repeat_id != "NULL":
+        db.execute_query(f'DELETE FROM repeated_reminders WHERE id = "{repeat_id}" AND userid = "{get_jwt_identity()}";')
+    else:
+        db.execute_query(f'DELETE FROM reminders WHERE reminder_id = "{repeat_id}" AND userid = "{get_jwt_identity()}";')
+    return "Event successfully deleted.", 200
+
---
+++ server/unit_tests/reminders_test.py
@@ -0,0 +1,78 @@
+import unittest
+import os
+from configparser import ConfigParser
+import requests
+
+
+class MyTestCase(unittest.TestCase):
+    def __init__(self):
+        super().__init__()
+
+        path = os.getcwd()
+        path = path.split(os.sep)
+        parent_path = os.sep.join(path[:-1])
+        config_path = parent_path + os.sep + 'app_config.ini'
+        config = ConfigParser()
+        config.read(config_path)
+
+        self.api_uri = f"http://{config['SERVER_INFO']['host']}:{config['SERVER_INFO']['port']}/api"
+        self.token = self.login()
+
+    def login(self):
+        uri = self.api_uri + '/login'
+        body = {
+            'email': 'test@tbd.com',
+            'password': 'test123'
+        }
+        response = requests.post(uri, data=body)
+        return response.json()['access_token']
+
+    def test_1_get_reminders(self):
+        uri = self.api_uri + '/reminders'
+
+        headers = {
+            'Authorization': f'Bearer {self.token}',
+            'Content-Type': 'application/json'
+        }
+
+        response = requests.get(uri, headers=headers)
+
+        reminders = dict(response.json()['reminders'])
+        self.assertEqual(len(reminders), 0)
+
+    def test_2_add_reminder(self):
+        uri = self.api_uri + '/reminders'
+
+        headers = {
+            'Authorization': f'Bearer {self.token}',
+            'Content-Type': 'application/json'
+        }
+        body = {
+            "title": "Test",
+            "description": "Test",
+            "start_time": "2080/01/01 00:00",
+            "end_time": "2080/01/02 00:00",
+            "reminder_type": 1,
+            "progress": 1
+        }
+
+        response = requests.post(uri, headers=headers, data=body)
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_3_get_reminders(self):
+        uri = self.api_uri + '/reminders'
+
+        headers = {
+            'Authorization': f'Bearer {self.token}',
+            'Content-Type': 'application/json'
+        }
+
+        response = requests.get(uri, headers=headers)
+
+        reminders = dict(response.json()['reminders'])
+        self.assertEqual(len(reminders), 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
