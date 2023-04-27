f64d5bc0230d060986c1a0d53faf13735a55c61d
Thomas Starnes <43146669+tstar98@users.noreply.github.com>
2023-04-26 18:12:17 -0600

    Created data collector that queries soccer API (#14)
    * Created data collector that queries soccer API
    
    Data collector queries football-data API and gets all upcoming matches for 10 different leagues around the world
    
    Added matches table with basic info on matches
    
    * Updated data_collecotr to grab correct start date and to default team names to TBD
    
    Scheduled matches with undetermined teams display team as TBD

--- server/db.py
+++ server/db.py
@@ -3,9 +3,11 @@ import sqlite3 as sql
 connection = sql.connect('calender.db', check_same_thread=False)
 cursor = connection.cursor()
 
+
 def execute_query(query):
     return cursor.execute(query)
 
-def execute_insert(query):
+
+def execute_commit(query):
     execute_query(query)
     connection.commit()
