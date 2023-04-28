57573103311201662e5f556dc8c3a533dcf9305b
Thomas Starnes <43146669+tstar98@users.noreply.github.com>
2023-04-28 15:12:39 -0600

    API end points for matches and the data analyzer (#15)
    * Changed identity to use userID
    
    * Created unit tests for reminder API
    
    * updated reminders API
    
    * updated reminders init
    
    * Update reminders.py
    
    * Update reminders_test.py
    
    * updated tested reminders
    
    * updated commit
    
    * Created data collector that queries soccer API
    
    Data collector queries football-data API and gets all upcoming matches for 10 different leagues around the world
    
    Added matches table with basic info on matches
    
    * Updated data_collecotr to grab correct start date and to default team names to TBD
    
    Scheduled matches with undetermined teams display team as TBD
    
    * Created user_preferences end points and added user_preferred_competitions table
    
    table stores all competitions user wants to track
    
    * Added index on user_id of reminders
    
    * Added /matches endpoint that returns all matches in competiton that user is following
    
    * Added helper function for reminders and /calendar endpoint
    
    * analyzer test
    
    * fixed typos
    
    * Fixed /calendar endpoint bug
    
    * Fixed another bug
    
    ---------
    
    Co-authored-by: Scott McCall <damc8609@colorado.edu>
    Co-authored-by: damc8609 <77517580+damc8609@users.noreply.github.com>

---
+++ server/matches.py
@@ -0,0 +1,72 @@
+from flask import Blueprint, request, jsonify
+from flask_jwt_extended import jwt_required, get_jwt_identity
+
+import db
+
+bp = Blueprint('matches', __name__)
+
+
+def get_user_matches(user_id):
+    query = f'SELECT c.name, m.start_date_time, m.home_team, m.away_team FROM matches m ' \
+            f'JOIN known_scheduled_matches AS ksm  ON m.id = ksm.id ' \
+            f'JOIN user_preferred_competitions AS upc ON m.competiton = upc.competition_code ' \
+            f'JOIN competitions c ON m.competiton = c.code ' \
+            f'WHERE user_id = {user_id};'
+    results = db.execute_query(query).fetchall()
+
+    if results is None:
+        return list()
+
+    matches = [{
+        'competition': result[0],
+        'start_date_time': result[1],
+        'home_team': result[2],
+        'away_team': result[3],
+    } for result in results]
+    return matches
+
+
+@bp.route('/competitions', methods=['GET'])
+def competitions():
+    comps = db.execute_query(f"SELECT code, name FROM competitions;").fetchall()
+    return dict(comps), 200
+
+
+@bp.route('/preferences', methods=['GET'])
+@jwt_required()
+def get_preferred_competitions():
+    result = db.execute_query(f"SELECT competition_code FROM user_preferred_competitions WHERE "
+                              f"user_id = {get_jwt_identity()};").fetchall()
+
+    if result is None:
+        return list(), 200
+
+    return list(code[0] for code in result), 200
+
+
+@bp.route('/preferences', methods=['POST'])
+@jwt_required()
+def add_preferred_competitions():
+    comps = list(request.form['competitions'])
+
+    for comp in comps:
+        db.execute_commit(f"INSERT INTO user_preferred_competitions(userid, competition_code) "
+                          f"VALUES({get_jwt_identity()}, '{comp}');")
+    return comps, 200
+
+
+@bp.route('/preferences', methods=['DELETE'])
+@jwt_required()
+def delete_preferred_competitions():
+    comps = tuple(request.form['competitions'])
+
+    db.execute_commit(f"DELETE FROM user_preferred_competitions WHERE user_id = {get_jwt_identity()} AND "
+                      f"competition_code in {comps};")
+    
+    return comps, 200
+
+
+@bp.route('/matches', methods=['GET'])
+@jwt_required()
+def matches():
+    return jsonify(get_user_matches(get_jwt_identity())), 200
---
+++ server/reminders.py
@@ -0,0 +1,83 @@
+import logging
+from configparser import ConfigParser
+from datetime import datetime, timedelta
+from flask import Flask, request, Blueprint, jsonify
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity, get_jwt
+
+from matches import get_user_matches
+import db
+
+bp = Blueprint('reminders', __name__, url_prefix='/reminders')
+
+
+def get_user_reminders(user_id):
+    result = db.execute_query(f'SELECT * FROM reminders WHERE userid = "{user_id}";').fetchall()
+    events = []
+    for event in result:
+        events.append(
+            {"reminder_id": event[0], "title": event[2], "description": event[3], "start_time": event[4],
+             "end_time": event[5], "reminder_type": event[6], "progress": event[7], "time_taken": event[8],
+             "repeat_id": event[9]})
+    return events
+
+
+@bp.route('/get_reminders')
+@jwt_required()
+def return_data():
+    return jsonify(get_user_reminders(get_jwt_identity())), 200
+
+
+@bp.route('/add_event', methods=['POST'])
+@jwt_required()
+def add_event():
+    # id = get_jwt_identity()
+    id = 1
+    title = request.form['title']
+    description = request.form['description']
+    start_time = request.form['start_time']
+    end_time = request.form['end_time']
+    reminder_type = request.form['reminder_type']
+    progress = request.form['progress']
+    db.execute_insert(
+        f'INSERT INTO reminders(userid, title, description, start_time_stamp, end_time_stamp, reminder_type, progress) VALUES("{id}", "{title}", "{description}", "{start_time}", "{end_time}", "{reminder_type}", "{progress}");')
+    return "Event successfully added.", 200
+
+
+@bp.route('/delete_event', methods=['POST', 'DELETE'])
+@jwt_required()
+def delete_event():
+    # id = get_jwt_identity()
+    id = 1
+    repeat_id = request.form['repeat_id']
+    reminder_id = request.form['reminder_id']
+    if repeat_id != "NULL":
+        db.execute_insert(f'DELETE FROM repeated_reminders WHERE id = "{repeat_id}" AND userid = "{id}";')
+        return "Repeated event successfully deleted.", 200
+    else:
+        db.execute_insert(f'DELETE FROM reminders WHERE reminder_id = "{reminder_id}" AND userid = "{id}";')
+        return "Event successfully deleted.", 200
+
+
+@bp.route('/calendar', methods=['GET'])
+@jwt_required()
+def calendar():
+    reminders = get_user_reminders(get_jwt_identity())
+    matches = get_user_matches(get_jwt_identity())
+
+    for match in matches:
+        for reminder in reminders:
+
+            date_format = "%Y-%m-%d %H:%M:%S"
+            reminder_start_time = datetime.strptime(reminder['start_time'], date_format)
+            match_start_time = datetime.strptime(match['start_date_time'], date_format)
+            reminder_end_time = datetime.strptime(reminder['end_time'], date_format)
+            match_end_time = match_start_time + timedelta(hours=2) #FIX
+
+            if (match_start_time < reminder_start_time < match_end_time) or (
+                    match_start_time < reminder_end_time < match_end_time):
+                matches.remove(match)
+
+    events = reminders + matches
+    return jsonify({"count": len(events), "events": events}), 200
+
+
