354382b282bdb9a6afe33bd690e998e26eaf0dc0
tstar98 <43146669+tstar98@users.noreply.github.com>
2023-05-04 15:41:32 -0600

    Update home.py

--- client/home.py
+++ client/home.py
@@ -1,16 +1,16 @@
 from flask import Blueprint, render_template
 
-home = Blueprint('home', __name__, template_folder='templates')
+home = Blueprint('home', __name__, template_folder='/templates/home')
 
 @home.route("/")
 @home.route('/home')
 def index():
-    return render_template('home/login.html')
+    return render_template('login.html')
 
 @home.route('/about')
 def about():
-    return render_template('home/about.html')
+    return render_template('about.html')
 
 @home.route('/contactUs')
 def contactUs():
-    return render_template('home/contactUs.html')
+    return render_template('contactUs.html')
