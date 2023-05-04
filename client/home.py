fc20649b1a727d3514512b0cc51bd26cd4685b69
tstar98 <43146669+tstar98@users.noreply.github.com>
2023-05-04 16:03:55 -0600

    Update home.py

--- client/home.py
+++ client/home.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template
 import os
 
-templates = os.path.dirname(__file__) + os.sep + "templates"
+templates = os.path.dirname(__file__) + os.sep + "templates" + os.sep
 
 home = Blueprint('home', __name__, template_folder=templates)
 
